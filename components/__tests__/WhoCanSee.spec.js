4803ea9690f9e8caa5abac399caa4e6d9bee2b86
TDAK <dbcgdhh@gmail.com>
2019-10-07 12:16:32 +0700

    Fix who can see test

--- components/__tests__/WhoCanSee.spec.js
+++ components/__tests__/WhoCanSee.spec.js
@@ -1,9 +1,15 @@
 import DashboardWhoCanSee from "../DashboardWhoCanSee.vue";
 
 describe("DashboardWhoCanSee.vue", () => {
-  it("emit update value on user id list change", () => {
+  it("emit update value on change selected agencies change", () => {
     const wrapper = shallowMount(DashboardWhoCanSee);
-    wrapper.vm.userIdList = [1, 2, 3];
+    wrapper.vm.selectedAgencies = [1, 2, 3];
+    expect(wrapper.emitted("update:value")).toBeTruthy();
+  });
+
+  it("emit update value on change selected freelancers change", () => {
+    const wrapper = shallowMount(DashboardWhoCanSee);
+    wrapper.vm.selectedFreelancers = [1, 2, 3];
     expect(wrapper.emitted("update:value")).toBeTruthy();
   });
 });
