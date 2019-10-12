609467a634ae64052165d32d267889ec5d562c1f
TDAK <dbcgdhh@gmail.com>
2019-10-12 23:42:02 +0700

    Modularize serach for media

--- pages/_lang/dashboard/search-for-media.vue
+++ pages/_lang/dashboard/search-for-media.vue
@@ -1,37 +1,59 @@
 <template>
-    <v-form ref="form" class="text-left">
-        <TheSearchForMediaSearchPanel
-            :countries="countries"
-            :media="mediaForSearchFilter"
-            :segments="segmentsForSearchFilter"
-            @removeCountry="removeFromSelectedCountries"
-            @removeMedia="removeFromSelectedMedia"
-            @removeSegment="removeFromSelectedSegments"
-        ></TheSearchForMediaSearchPanel>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="12" sm="2">
+                <div class="text-left pr-7">
+                    <button
+                        class="clear-filter mb-4 primary--text body-2"
+                        type="button"
+                        @click="clearFilter"
+                    >
+                        <v-icon left small color="primary">mdi-filter-remove</v-icon>
+                        {{ $t('search_user.clear_filter') }}
+                    </button>
+                </div>
 
-        <TheSearchForMediaSearchSelector @change="updateSearchValues"></TheSearchForMediaSearchSelector>
+                <v-form ref="form" class="text-left filter-panel pr-7">
+                    <DashboardSearchFilters
+                        showCountries
+                        showCities
+                        showSegments
+                        showMedia
+                        showLanguages
+                        @change="updateSearchValues"
+                    ></DashboardSearchFilters>
+                </v-form>
+            </v-col>
 
-        <div class="d-flex justify-start">
-            <v-btn
-                color="primary"
-                class="px-8"
-                ref="postButton"
-                :loading="searching"
-                :disabled="searching"
-                @click="doSearchAction"
-            >
-                {{$t("common.search")}}
-                <v-icon right dark>mdi-magnify</v-icon>
-            </v-btn>
-        </div>
-    </v-form>
+            <v-col cols="12" sm="10">
+                <v-text-field
+                    label="Type something here"
+                    outlined
+                    single-line
+                    append-icon="mdi-magnify"
+                    class="mb-4"
+                    @input="doSearchAction"
+                    hide-details
+                ></v-text-field>
+
+                <DashboardSearchPanel :items="searchPanelItems" @remove="removeFromSearchPanel"></DashboardSearchPanel>
+
+                <DashboardSearchResultWrapper
+                    v-show="users.length"
+                    :users="users"
+                    :value.sync="userIdList"
+                    chip-field-name="services"
+                ></DashboardSearchResultWrapper>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
 import mixinDashboardTitle from "~/mixins/dashboard-title";
-
-import TheSearchForMediaSearchPanel from "@/components/TheSearchForMediaSearchPanel";
-import TheSearchForMediaSearchSelector from "@/components/TheSearchForMediaSearchSelector";
+import DashboardSearchResultWrapper from "@/components/DashboardSearchResultWrapper";
+import DashboardSearchFilters from "@/components/DashboardSearchFilters";
+import DashboardSearchPanel from "@/components/DashboardSearchPanel";
 
 export default {
     name: "SearchForMedia",
@@ -39,8 +61,9 @@ export default {
     mixins: [mixinDashboardTitle],
 
     components: {
-        TheSearchForMediaSearchPanel,
-        TheSearchForMediaSearchSelector
+        DashboardSearchResultWrapper,
+        DashboardSearchFilters,
+        DashboardSearchPanel
     },
 
     data() {
@@ -48,19 +71,66 @@ export default {
             searching: false,
 
             countries: [],
+            cities: [],
             segments: [],
-            language: "",
-            media: []
+            media: [],
+            languages: [],
+
+            userIdList: [],
+
+            users: [
+                {
+                    _id: "haha",
+                    name: "hihi",
+                    rating: 4.5,
+                    avatar: "https://cdn.vuetifyjs.com/images/john.jpg",
+                    company: "APMediahub",
+                    job_title: "Web Designer",
+                    full_name: "Tran Duy An Khuong",
+                    services: ["hahaa", "HTML5", "Audio/Video Production"],
+                    website: "https://www.google.com"
+                },
+                {
+                    _id: "haha1",
+                    name: "hihidfafd",
+                    rating: 4,
+                    avatar:
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwPcapaC3wvjcgD8eG-kv5yDba2WHNRrpoNujxdDwiH2W6enlU",
+                    company: "Twice",
+                    job_title: "Singer",
+                    full_name: "Chou Tzu-yu",
+                    services: ["Beautiful", "Angel", "Sing Shoot Dance"],
+                    facebook: "https://www.facebook.com/TzuyuChou.fc/"
+                }
+            ]
         };
     },
 
     computed: {
         title() {
-            return this.$t("common.search") + " " + this.$t("common.media");
+            return this.$t("common.search") + " " + this.$t("user.role.media");
         },
 
-        inputBorderColor() {
-            return this.$store.state.inputBorderColor;
+        searchPanelItems() {
+            return {
+                countries: this.countriesForSearchFilter,
+                cities: this.citiesForSearchFilter,
+                segments: this.segmentsForSearchFilter,
+                media: this.mediaForSearchFilter,
+                languages: this.languagesForSearchFilter
+            };
+        },
+
+        countriesForSearchFilter() {
+            return this.countries.map(country => {
+                return { text: country, value: country };
+            });
+        },
+
+        citiesForSearchFilter() {
+            return this.cities.map(city => {
+                return { text: city, value: city };
+            });
         },
 
         segmentsForSearchFilter() {
@@ -91,35 +161,54 @@ export default {
                     value: key
                 };
             });
+        },
+
+        languagesForSearchFilter() {
+            return this.languages.map(language => {
+                return {
+                    text: language,
+                    value: language
+                };
+            });
         }
     },
 
     methods: {
-        removeFromSelectedCountries(country) {
-            const index = this.countries.indexOf(country);
-            this.countries.splice(index, 1);
-        },
-
-        removeFromSelectedSegments(segment) {
-            const index = this.segments.indexOf(segment);
-            this.segments.splice(index, 1);
-        },
-
-        removeFromSelectedMedia(value) {
-            const index = this.media.indexOf(value);
-            this.media.splice(index, 1);
+        removeFromSearchPanel({ key, value }) {
+            const index = this[key].indexOf(value);
+            this[key].splice(index, 1);
         },
 
         updateSearchValues({ key, value }) {
             this[key] = value;
         },
 
+        clearFilter() {
+            this.countries.splice(0);
+            this.cities.splice(0);
+            this.segments.splice(0);
+            this.media.splice(0);
+            this.languages.splice(0);
+        },
+
         async doSearchAction() {
-            const result = await this.$axios.$get(
-                "https://jsonplaceholder.typicode.com/todos/1"
-            );
-            console.log(result);
+            const users = [{ _id: "haha", name: "hihi" }];
+            this.users = [...users];
         }
     }
 };
 </script>
+
+<style lang="scss" scoped>
+.filter-panel {
+    border-right: 1px solid #e0e0e0;
+}
+
+.clear-filter {
+    border-bottom: 1px solid transparent;
+
+    &:hover {
+        border-bottom: 1px solid #ccc;
+    }
+}
+</style>
