2562fed142355b2883006720b1e0522dc1dc17b9
TDAK <dbcgdhh@gmail.com>
2019-11-01 16:52:20 +0700

    For interview

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,79 +1,92 @@
 export default {
-  mode: "universal",
+    mode: "universal",
 
-  head: {
-    titleTemplate: "%s",
-    title: "APMediahub",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: process.env.npm_package_description || ""
-      }
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i&display=swap&subset=vietnamese"
-      }
-    ]
-  },
+    head: {
+        titleTemplate: "%s",
+        title: "APMediahub",
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            },
+            {
+                hid: "description",
+                name: "description",
+                content: process.env.npm_package_description || ""
+            }
+        ],
+        link: [
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            {
+                rel: "stylesheet",
+                href:
+                    "https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i&display=swap&subset=vietnamese"
+            }
+        ]
+    },
 
-  loading: { color: "#fff" },
+    loading: { color: "#fff" },
 
-  css: ["~/assets/page-transition.css"],
+    css: ["~/assets/page-transition.css"],
 
-  plugins: ["~/plugins/global_components.js"],
+    plugins: ["~/plugins/global_components.js"],
 
-  buildModules: ["@nuxtjs/vuetify"],
+    buildModules: ["@nuxtjs/vuetify"],
 
-  modules: ["@nuxtjs/axios", "nuxt-i18n"],
+    modules: ["@nuxtjs/axios", "nuxt-i18n"],
 
-  axios: {
-    browserBaseURL: "http://localhost:5000"
-  },
+    axios: {
+        browserBaseURL: "http://localhost:5000"
+    },
 
-  i18n: {
-    locales: ["vn", "en"],
-    defaultLocale: "vn",
-    strategy: "prefix_except_default",
-    vueI18nLoader: true,
-    vueI18n: {
-      fallbackLocale: "vn",
-      messages: {
-        vn: {
-          welcome: "Xin chào"
-        },
+    i18n: {
+        locales: ["vn", "en"],
+        defaultLocale: "vn",
+        strategy: "prefix_except_default",
+        vueI18nLoader: true,
+        vueI18n: {
+            fallbackLocale: "vn",
+            messages: {
+                vn: {
+                    user_role: {
+                        client: "Nhà Tuyển Dụng",
+                        agency: "Nhà Môi Giới",
+                        journalist: "Nhà Báo",
+                        freelancer: "Người Làm Việc Tự Do"
+                    }
+                },
 
-        en: {
-          welcome: "Welcome"
+                en: {
+                    user_role: {
+                        client: "Client",
+                        agency: "Agency",
+                        journalist: "Journalist",
+                        freelancer: "Freelancer"
+                    }
+                }
+            }
         }
-      }
-    }
-  },
+    },
 
-  vuetify: {
-    customVariables: [],
-    theme: {
-      dark: false,
-      themes: {
-        light: {
-          primary: "#c80000"
+    vuetify: {
+        customVariables: [],
+        theme: {
+            dark: false,
+            themes: {
+                light: {
+                    primary: "#c80000"
+                }
+            }
         }
-      }
+    },
+
+    generate: {
+        routes: ["/", "/login", "/en", "/en/login"]
+    },
+
+    build: {
+        extend(config, context) {},
+        standalone: true
     }
-  },
-
-  generate: {
-    routes: ["/", "/login", "/en", "/en/login"]
-  },
-
-  build: {
-    extend(config, context) {},
-    standalone: true
-  }
 };
