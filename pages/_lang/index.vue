eee85db9f4b2690caccd903db734ad69b441038f
TDAK <dbcgdhh@gmail.com>
2019-11-03 14:13:49 +0700

    Still moving vue-i18n

---
+++ pages/_lang/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <main>
+        <h1>HOME</h1>
+
+        <nuxt-link :to="`/${this.locale}/dashboard`">Dashboard</nuxt-link>
+        <br />
+        <nuxt-link :to="`/${this.locale}/login`">Login</nuxt-link>
+        <br />
+        <nuxt-link :to="`/${this.locale}/register`">Register</nuxt-link>
+        <br />
+    </main>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+    name: "Home",
+    computed: {
+        ...mapState(["locale"])
+    }
+};
+</script>
