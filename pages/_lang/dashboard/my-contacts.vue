829e28a305e09b3e2068559d0b81f59148bbbc2c
TDAK <dbcgdhh@gmail.com>
2019-11-12 11:28:24 +0700

    Fix my contacts list and title bug

--- pages/_lang/dashboard/my-contacts.vue
+++ pages/_lang/dashboard/my-contacts.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <CreateModal @save="createNewList"></CreateModal>
-        <ContactList v-if="currentUser" :list="contactList" class="mt-5" @delete="deleteList"></ContactList>
-    </div>
+  <div>
+    <CreateModal @save="createNewList"></CreateModal>
+    <ContactList v-if="currentUser" :list="contactList" class="mt-5" @delete="deleteList"></ContactList>
+  </div>
 </template>
 
 <script>
@@ -13,77 +13,61 @@ import mixinApi from "@/mixins/api";
 import { mapState } from "vuex";
 
 export default {
-    name: "MyContacts",
+  name: "MyContacts",
 
-    mixins: [mixinDashboardTitle, mixinApi],
+  mixins: [mixinDashboardTitle, mixinApi],
 
-    components: {
-        CreateModal,
-        ContactList
+  components: {
+    CreateModal,
+    ContactList
+  },
+
+  data() {
+    return {
+      newContactList: []
+    };
+  },
+
+  computed: {
+    ...mapState(["currentUser"]),
+
+    title() {
+      return this.$t("dashboard.my_contacts");
     },
 
-    data() {
-        return {
-            newContactList: [],
-            currentContactList: []
-        };
+    currentContactList() {
+      if (!this.currentUser) return [];
+      return [...this.currentUser.contact_list];
     },
 
-    computed: {
-        ...mapState(["currentUser"]),
-
-        title() {
-            return this.$t("title");
-        },
-
-        contactList() {
-            return [...this.newContactList, ...this.currentContactList];
-        }
-    },
-
-    watch: {
-        currentUser(newValue, oldValue) {
-            if (newValue) {
-                this.currentContactList = [...this.currentUser.contact_list];
-            }
-        }
-    },
-
-    methods: {
-        async createNewList(value) {
-            const list = {
-                id: this.currentUser.contact_list.length + 1,
-                name: value,
-                users: []
-            };
-
-            this.newContactList.push(list);
-
-            await this.$api_updateContactList(this.contactList);
-        },
-
-        async deleteList(listId) {
-            this.newContactList = this.newContactList.filter(
-                item => item.id != listId
-            );
-            this.currentContactList = this.currentContactList.filter(
-                item => item.id != listId
-            );
-
-            await this.$api_updateContactList(this.contactList);
-        }
+    contactList() {
+      return [...this.newContactList, ...this.currentContactList];
     }
+  },
+
+  methods: {
+    async createNewList(value) {
+      const list = {
+        id: this.currentUser.contact_list.length + 1,
+        name: value,
+        users: []
+      };
+
+      this.newContactList.push(list);
+
+      await this.$api_updateContactList(this.contactList);
+    },
+
+    async deleteList(listId) {
+      this.newContactList = this.newContactList.filter(
+        item => item.id != listId
+      );
+      this.currentContactList = this.currentContactList.filter(
+        item => item.id != listId
+      );
+
+      await this.$api_updateContactList(this.contactList);
+    }
+  }
 };
 </script>
-
-<i18n>
-{
-    "en": {
-        "title": "My contacts"
-    },
-
-    "vn": {
-        "title": "Danh bạ của tôi"
-    }
-}
-</i18n>
